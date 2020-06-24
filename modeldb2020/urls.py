mcdougallab/modeldb

commit e1597d822d43906fdb90f1c3759f2d8bd219dc93

Initial API
Mostly addresses #9

Not closing that because I would like to have a Swagger presentation, maybe some optional
parameters to change how the result is returned (e.g. a flag for indenting/pretty-printing),
and maybe some search capability.

modeldb2020/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-from . import views
+from django.urls import path, re_path, include
+from . import views, urls_api_v1
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -25,6 +25,7 @@ urlpatterns = [
     re_path(r'^(?:modeldb/)?search(?:fulltext)?(?:\.(?:cs)?html)?$(?i)', views.search, name='search'),
     re_path(r'^(?:modeldb/)?neuron_dwnldguide(?:\.(?:cs)?html)?$(?i)', views.static, {'page': 'neuron_download_guide', 'title': 'NEURON download help'}, name='neuron_download_guide'),
     re_path(r'^(?:modeldb/)?howtocite(?:\.(?:cs)?html)?$(?i)', views.static, {'page': 'howtocite', 'title': 'How to cite ModelDB'}, name='howtocite'),
+    re_path(r'^(?:modeldb/)?api(?:\.(?:cs)?html)?$(?i)', views.static, dict(page='apidocs', title='The ModelDB API'), name='apidocs'),
     re_path(r'^(?:modeldb/)?(?:mdb)?resources(?:\.(?:cs)?html)?$(?i)', views.static, dict(page='mdbresources', title='Web resources for ModelDB'), name='mdbresources'),
     re_path(r'^(?:modeldb/)?eco(?:\.(?:cs)?html)?$(?i)', views.static, dict(page='eco', title='An ecosystem of computational neuroscience resources'), name='eco'),
     re_path(r'^(?:modeldb/)?listbymodelname(?:\.(?:cs)?html)?$(?i)', views.listbymodelname, name='listbymodelname'),
@@ -43,5 +44,6 @@ urlpatterns = [
     re_path(r'^(?:modeldb/)?findbysimulator(?:\.(?:cs)?html)?$(?i)', views.findbysimulator, name='findbysimulator'),
     re_path(r'^(?:modeldb/)?(?:mdb)?citations(?:\.(?:cs)?html)?$(?i)', views.mdbcitations, name='mdbcitations'),
     #path('admin/', admin.site.urls),
+    re_path(r'^api/v1/(?i)', include(urls_api_v1)),
     path('<slug:model_id>', views.showmodel_redirect, name='showmodel_redirect')
 ]

modeldb2020/api_v1.py
@@ -1,0 +1,80 @@
+import json
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.clickjacking import xframe_options_exempt
+from . import models
+from .models import currents, genes, regions, receptors, transmitters, simenvironments, modelconcepts, modeltypes, celltypes, papers
+
+
+ModelDB = models.ModelDB()
+
+def _output(request, data):
+    # TODO: allow request for indentation
+    if isinstance(data, dict):
+        data2 = {key: value for key, value in data.items() if key != '_id'}
+    else:
+        data2 = data
+    return HttpResponse(json.dumps(data2))
+
+def index(request):
+    return _output(request, sorted([
+        'models', 'celltypes', 'currents', 'genes', 'regions', 'receptors', 'transmitters',
+        'simenvironments', 'modelconcepts', 'modeltypes', 'papers'
+        ]))
+
+def models_view(request, model_id=None, field=None):
+    if model_id is not None:
+        model_id = str(model_id)
+        if ModelDB.has_model(model_id):
+            return _output(request, ModelDB.model(model_id)._model)
+        else:
+            return HttpResponse("404 Not Found", status=404)
+    elif field is not None:
+        return _output(request, [item.get(field) for item in models.modeldb.values()])
+    else:
+        return _output(request, list(models.modeldb.keys()))
+
+def papers_view(request, _id=None, field=None):
+    return _generic_view(request, papers, _id, field)
+
+def modeltypes_view(request, _id=None, field=None):
+    return _generic_view(request, modeltypes, _id, field)
+
+def modelconcepts_view(request, _id=None, field=None):
+    return _generic_view(request, modelconcepts, _id, field)
+
+def celltypes_view(request, _id=None, field=None):
+    return _generic_view(request, celltypes, _id, field)
+
+def currents_view(request, _id=None, field=None):
+    return _generic_view(request, currents, _id, field)
+
+def genes_view(request, _id=None, field=None):
+    return _generic_view(request, genes, _id, field)
+
+def regions_view(request, _id=None, field=None):
+    return _generic_view(request, regions, _id, field)
+
+def receptors_view(request, _id=None, field=None):
+    return _generic_view(request, receptors, _id, field)
+
+def transmitters_view(request, _id=None, field=None):
+    return _generic_view(request, transmitters, _id, field)
+
+def simenvironments_view(request, _id=None, field=None):
+    return _generic_view(request, simenvironments, _id, field)
+
+
+def _generic_view(request, collection, _id, field):
+    if _id is not None:
+        if _id in collection:
+            return _output(request, collection[_id])
+        elif str(_id) in collection:
+            return _output(request, collection[str(_id)])
+        else:
+            return HttpResponse("404 Not Found", status=404)
+    elif field is not None:
+        return _output(request, [item.get(field) for item in collection.values()])
+    else:
+        return _output(request, list(collection.keys()))

modeldb2020/templates/apidocs.html
@@ -1,0 +1,30 @@
+{% extends "main.html" %}
+
+{% block content %}
+    <center><h3>ModelDB - API Overview</h3></center>
+
+    <p>
+        The prefix for accessing all API calls is <code>/api/v1</code>.
+    </p>
+
+    <p>
+        To see what types of data are available simply call <a href="/api/v1">/api/v1</a>. 
+        This returns a JSON-encoded list that includes e.g. celltypes, models, ...
+    </p>
+
+    <p>
+        To find out which data objects are available for a given type, query that type.
+        e.g. The list of all model object ids is <a href="/api/v1/models">/api/v1/models</a>.
+    </p>
+
+    <p>
+        To get a specific instance, include the object id. e.g. The data for model 87284 is at
+        <a href="/api/v1/models/87284">/api/v1/models/87284</a>.
+    </p>
+
+    <p>
+        You can also get a list of property values in the same order as the object ids by
+        querying on the name of the property. For example, to get the "name" property for all
+        the celltypes objects, query <a href="/api/v1/celltypes/name">/api/v1/celltypes/name</a>.
+    </p>
+{% endblock %}

modeldb2020/templates/main.html
@@ -182,6 +182,7 @@
                     <li class="dropdown-item"><a href="/search">Advanced search</a></li>
                     <li class="dropdown-item"><a href="/help">Help</a></li>
                     <hr style="width:75%"/>
+                    <li class="dropdown-item"><a href="/api">API documentation</a></li>
                     <li class="dropdown-item"><a href="#">SenseLab mailing list</a></li>
                     <li class="dropdown-item"><a href="/mdbresources">Related resources</a></li>
                     <li class="dropdown-item"><a href="/eco">Computational Neuroscience ecosystem</a></li>

modeldb2020/urls_api_v1.py
@@ -1,0 +1,41 @@
+
+from django.contrib import admin
+from django.urls import path, re_path, include
+from . import api_v1
+
+urlpatterns = [
+    path('', api_v1.index),
+    path('models/<int:model_id>', api_v1.models_view),
+    path('models/<slug:field>', api_v1.models_view),
+    path('models', api_v1.models_view),
+    path('celltypes/<int:_id>', api_v1.celltypes_view),
+    path('celltypes/<slug:field>', api_v1.celltypes_view),
+    path('celltypes', api_v1.celltypes_view),
+    path('currents/<int:_id>', api_v1.currents_view),
+    path('currents/<slug:field>', api_v1.currents_view),
+    path('currents', api_v1.currents_view),
+    path('genes/<int:_id>', api_v1.genes_view),
+    path('genes/<slug:field>', api_v1.genes_view),
+    path('genes', api_v1.genes_view),
+    path('regions/<int:_id>', api_v1.regions_view),
+    path('regions/<slug:field>', api_v1.regions_view),
+    path('regions', api_v1.regions_view),
+    path('receptors/<int:_id>', api_v1.receptors_view),
+    path('receptors/<slug:field>', api_v1.receptors_view),
+    path('receptors', api_v1.receptors_view),
+    path('transmitters/<int:_id>', api_v1.transmitters_view),
+    path('transmitters/<slug:field>', api_v1.transmitters_view),
+    path('transmitters', api_v1.transmitters_view),
+    path('simenvironments/<int:_id>', api_v1.simenvironments_view),
+    path('simenvironments/<slug:field>', api_v1.simenvironments_view),
+    path('simenvironments', api_v1.simenvironments_view),
+    path('modelconcepts/<int:_id>', api_v1.modelconcepts_view),
+    path('modelconcepts/<slug:field>', api_v1.modelconcepts_view),
+    path('modelconcepts', api_v1.modelconcepts_view),
+    path('modeltypes/<int:_id>', api_v1.modeltypes_view),
+    path('modeltypes/<slug:field>', api_v1.modeltypes_view),
+    path('modeltypes', api_v1.modeltypes_view),
+    path('papers/<int:_id>', api_v1.papers_view),
+    path('papers/<slug:field>', api_v1.papers_view),
+    path('papers', api_v1.papers_view)
+]

commit 14c1880e94f6f154b6275a97c598b36aaaf0f920

Fix for short links.
Was broken by e1597d8

Difficulty: the API is now no longer case insensitive. (Was getting the non-reversible
regex error.)

modeldb2020/urls.py
@@ -44,6 +44,6 @@ urlpatterns = [
     re_path(r'^(?:modeldb/)?findbysimulator(?:\.(?:cs)?html)?$(?i)', views.findbysimulator, name='findbysimulator'),
     re_path(r'^(?:modeldb/)?(?:mdb)?citations(?:\.(?:cs)?html)?$(?i)', views.mdbcitations, name='mdbcitations'),
     #path('admin/', admin.site.urls),
-    re_path(r'^api/v1/(?i)', include(urls_api_v1)),
+    re_path(r'^api/v1/', include(urls_api_v1)),
     path('<slug:model_id>', views.showmodel_redirect, name='showmodel_redirect')
 ]