422ddcfdbce95c33954b2a9832fce86e49ea90d7
Robert A. McDougal <robert.mcdougal@yale.edu>
2020-07-08 15:10:11 +0000

    conf instructions for bitnami apache

--- modeldb2020/wsgi.py
+++ modeldb2020/wsgi.py
@@ -4,10 +4,13 @@ WSGI config for modeldb2020 project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+sys.path.append('/opt/bitnami/apps/django/django_projects/Project')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/Project/egg_cache")
+
 
 from django.core.wsgi import get_wsgi_application
 
