41a2b2300454185c69a435c73212aa1ec1d8741a
Robert A McDougal <robert.mcdougal@yale.edu>
2023-05-29 22:53:55 +0000

    code for pulling biosimulations models

---
+++ extract_data/pull_biosimulations.py
@@ -0,0 +1,23 @@
+import requests
+import re
+import json
+from pymongo import MongoClient
+
+SETTINGS_PATH = "/home/bitnami/modeldb-settings.json"
+PIPELINE_SETTINGS_PATH = "/home/bitnami/app-settings.json"
+
+with open(SETTINGS_PATH) as f:
+    security = json.load(f)
+
+with open(PIPELINE_SETTINGS_PATH) as f:
+    pipeline_settings = json.load(f)
+
+mongodb = MongoClient()
+sdb = mongodb[security["db_name"]]
+sdb.authenticate(security["mongodb_user"], security["mongodb_pw"])
+
+data = requests.get("https://raw.githubusercontent.com/biosimulations/biosimulations-modeldb/dev/biosimulations_modeldb/final/status.yml").text
+models = re.findall("modeldb-(\d+)", data)
+
+for model in models:
+    sdb.models.update_one({"id": int(model)}, {"$set": {"biosimulations": {"value": f"modeldb-{model}"}}})
